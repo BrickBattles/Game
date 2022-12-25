bcddd8447fffcaec39ea641e6e94e5cd1f017ee6
Jonathan Conn <jonathansconn@gmail.com>
2022-12-25 14:58:36 -0500

    re structured project, moved game page into game component and passed parameters

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,48 @@
-import { SocketAddress } from "net";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
-import { v4 as uuidv4, validate } from "uuid";
-import MatchTable from "../components/matchTable";
+import MatchTable from "../components/web/matchTable";
+import Game from "../components/game/game";
 
-import { Match, MatchStorage, Player, PlayerStorage} from "../customTypes/game";
+import {
+  Match,
+  MatchStorage,
+  Player,
+  PlayerStorage,
+} from "../customTypes/game";
 import { MatchState, PlayerState } from "../customTypes/states";
 
-import { useRouter } from "next/router";
-
-let socket: Socket;
 
 const Home = () => {
-  const router = useRouter();
-  let [matchData, setMatchData] = useState<MatchStorage>({});
+  
+  let [socket, setSocket] = useState<Socket>();
+  let [matchData, setMatchData] = useState<MatchStorage>({});  
+  let [matchID, setMatchID] = useState<string>("");
 
-    
   useEffect(() => {
     const socketInitializer = async () => {
       await fetch("/api/socket");
-      socket = io({
-        forceNew: true,
-      });
+      setSocket(socket = io({forceNew: true,}));
 
-      socket.on("connect", () => {        
-        socket.emit("join_game", { id: '', data: "player data", address: '0x0012324', state: PlayerState.NEW});
+      socket.on("connect", () => {
+        socket!.emit("join_game", {
+          id: "",
+          data: "player data",
+          address: "0x0012324",
+          state: PlayerState.NEW,
+        });
         // socket.emit("join", { data: "player data", address: '0x00123', state: PlayerState.NEW});
 
-        socket.on("update_matchData", (curMatches:{[key: string]: Match}) => {          
-          console.log("update_matchData", curMatches)
-          setMatchData(curMatches);          
-        });
+        socket!.on(
+          "update_matchData",
+          (curMatches: { [key: string]: Match }) => {
+            console.log("update_matchData", curMatches);
+            setMatchData(curMatches);
+          }
+        );
 
-        socket.on("start_match", (matchID: string) => {          
-          router.push(`/game`);
+        socket!.on("start_match", (matchID: string) => {
+          setMatchID(matchID);
         });
-
       });
     };
 
@@ -43,31 +50,30 @@ const Home = () => {
   }, []);
 
   const createMatch = () => {
-    let m: Match = {id: "123", playerData: {}, state: MatchState.NEW}
-    socket.emit("create_match", m);    
-  };
-  
-  const joinMatch = (matchId: string) => {
-    socket.emit("join_match", matchId);
+    let m: Match = { id: "123", playerData: {}, state: MatchState.NEW };
+    socket!.emit("create_match", m);
   };
 
-  const test = () => {
-    setMatchData({test: {id: "123", playerData: {}, state: MatchState.NEW}})
-  }
+  const joinMatch = (matchId: string) => {
+    socket!.emit("join_match", matchId);
+  };
 
   return (
     <div>
-      <button className="btn" onClick={createMatch}>
-        Create
-      </button>
+      {matchID ? (
+        <div>
+          <Game matchID={matchID} socket={socket!}/>
+        </div>
+      ) : (
+        <div>
+          <button className="btn" onClick={createMatch}>
+            Create
+          </button>
 
-    <button className="btn">
-      test
-      </button>
-      
-      <MatchTable data={matchData} join={joinMatch} />          
-
-    </div>          
+          <MatchTable data={matchData} join={joinMatch} />
+        </div>
+      )}
+    </div>
   );
 };
 
